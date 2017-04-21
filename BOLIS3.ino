78ffb7ec09996887a975ff50ff62505a62af9312
Edgar Solis Vizcarra <edgar21dejunio@gmail.com>
2017-04-20 21:28:05 -0500

    Empty start

---
+++ BOLIS3.ino
@@ -0,0 +1,24 @@
+//**************************************************************//
+//  Name    : BOCRE
+//  Author  : Edgar Solis Vizcarra
+//  Date    : 20 Mar, 2017
+//  Modified: 20 Apr, 2017
+//  Version : 1.0
+//  Notes   : Code for controlling a 2 channel solid state relay
+//          : with a user defined variable time on each channel. 
+//          : it is activated with a signal obtained from a sensor
+//          : and the time each is on is shown in a 3 digit display
+//          : with the two displays being controlled with a MAX 7219
+//          : led controller
+//****************************************************************
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
